f98ec1e406f308a88ce9ac9f9ccdb784eb20bed4
away <wjwddzyx@163.com>
2025-11-12 01:56:52 +0800

    update ssmgr and router

--- src/core/router.h
+++ src/core/router.h
@@ -5,35 +5,107 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <set>
+#include "errc.h"
+#include "leptjson.h"
 
 namespace uno
 {
-    class Router {
+    void load_role_req(int uid,
+                       const std::string& callback,
+                       const lept_value* callback_data);
+    void create_role_req(int uid,
+                        const std::string&  nick,
+                        const std::string& callback,
+                        const lept_value* callback_data);
+    void save_role_req(int uid,
+                        const lept_value& data,
+                        const lept_value& summary,
+                        const std::string& callback,
+                        const lept_value* callback_data);
+    void load_role_rsp(ErrCode ret,
+                       const std::string& nickname,
+                       const lept_value& summary,
+                       const lept_value& data,
+                       int uid);
+    void create_role_rsp(ErrCode ret, int uid);
+    void round_flow(int uid, int state);
+    void save_role_rsp(ErrCode ret, int uid);
+
+
+    class Router
+    {
     public:
-        Router() = default;
-        ~Router() = default;
+        static Router& router()
+        {
+            static Router instance;
+            return instance;
+        }
+
+        std::set<std::string> c2s;
+        std::set<std::string> s2s;
 
         template<typename... Args>
-        void call(const std::string& funcname, Args&&... args)
+        void call_s2s(const std::string& funcname, Args&&... args)
         {
-            std::cout << "call function: " << funcname << std::endl;
-            try
+            if (s2s.find(funcname) == s2s.end())
             {
-                if (funcname == "add")
-                {
-                    // todo function apply
-                }
-                else
-                {
-                    std::cerr << "unknown function: " << funcname << std::endl;
-                }
-            } catch (const std::exception& e)
-            {
-                std::cerr << "Err while dispatch s2s msg: " << e.what() << std::endl;
+                std::cerr << "Unabled internal msg " << funcname << std::endl;
+                return ;
             }
 
+#define CALL_FN(func) if(funcname == #func) { std::invoke(func, std::forward<Args>(args)...);  }
 
+            try
+            {
+                CALL_FN(load_role_req)
+                CALL_FN(create_role_req)
+                CALL_FN(save_role_req)
+                CALL_FN(load_role_rsp)
+                CALL_FN(create_role_rsp)
+                CALL_FN(round_flow)
+                CALL_FN(save_role_rsp)
+            } catch (const std::exception& e)
+            {
+                std::cerr << "Error while dispatch s2s msg " << funcname << std::endl;
+                std::cerr << e.what() << std::endl;
+            }
+
+#undef CALL_FN(func)
         }
+
+
+        bool contain_s2s(const std::string& funcname)
+        {
+            return (s2s.find(funcname) == s2s.end()) == false;
+        }
+
+        bool contain_c2s(const std::string& funcname)
+        {
+            return (c2s.find(funcname) == c2s.end()) == false;
+        }
+
+
+    private:
+
+
+        static bool is_created;
+
+        Router()
+        {
+            is_created = true;
+            s2s = {
+                "load_role_req",
+                "create_role_req",
+                "save_role_req",
+                "load_role_rsp",
+                "create_role_rsp",
+                "round_flow",
+                "save_role_rsp",
+            };
+        }
+        ~Router() = default;
+
     };
 }
 
--- src/core/ssmgr.h
+++ src/core/ssmgr.h
@@ -6,11 +6,35 @@
 
 #include "session.h"
 #include "router.h"
+#include <unordered_map>
+
+#define SESSION_ITER_COUNT    10
+#define SESSION_SAVE_DURATION (5 * 60 * 1000)
 
 namespace uno
 {
+    using SessionPtr = std::shared_ptr<Session>;
     class Ssmgr {
 
+
+
+    private:
+        std::vector<Session*> m_sessions;
+        std::unordered_map<int, SessionPtr> m_uid2session;
+        size_t m_iter = 0;
+
+        void on_error(const SessionPtr& session, const std::exception& err)
+        {
+            std::cerr << err.what() << std::endl;
+        }
+
+        template<typename... Args>
+        void on_c2s_msg(const SessionPtr& session,
+                        const std::string& funcname,
+                        Args&&... args)
+        {
+
+        }
     };
 
 }
